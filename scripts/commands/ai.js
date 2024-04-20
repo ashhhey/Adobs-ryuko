4d64a7c11ef090a15dd9b094fc3d945f710b313d
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-04-21 06:29:14 +0800

    Update ai.js

--- scripts/commands/ai.js
+++ scripts/commands/ai.js
@@ -1,34 +1,29 @@
-/* 
-If you encounter any errors, please give me feedback. Contact me on facebook https://facebook.com/joshg101
-*/
-
-const { get } = require('axios');
-let url = "https://ai-tools.replit.app";
-
-module.exports.config = {
-  name: "ai",
-  version: "1.0.0",
-  hasPermission: 0,
-  credits: "unknown",
-  description: "OpenAI official AI with no prefix",
-  commandCategory: "education",
-  usePrefix: true,
-  usages: "...",
-  cooldowns: 0
-};
-
-module.exports.run = async function({ api, event, args }) {
-    function sendMessage(msg) {
-        api.sendMessage(msg, event.threadID, event.messageID);
-    }
-    if (!args[0]) return sendMessage('Please provide a question first.');
-    const prompt = args.join(" ");
-    try {
-        api.sendMessage("Please bear with me while I ponder your request...", event.threadID, event.messageID);
-        const response = await get(`${url}/gpt?prompt=${encodeURIComponent(prompt)}&uid=${event.senderID}`);
-        const data = response.data;
-        return sendMessage(`${data.gpt4}\n\ncredits: www.facebook.com/mark.dev69`);
-    } catch (error) {
-        return sendMessage(error.message);
+const {get} = require('axios');
+const url = "https://combined-api-a2153f3cf0b5.herokuapp.com";
+module.exports = {
+    config: {
+        name: "ai",
+        aliases: ["gpt4","Gpt-4","gpt"],
+        version: "1.0.0",
+        role: 0,
+        credits: "Mark Hitsuraan",
+        info: "Chat with GPT4 Continues Conversation",
+        usage: "Gpt4 [text] to clear conversation type [Gpt4 Clear]",
+        cd: 0,
+    },
+    run: async function({api, event, args}){
+            let prompt = args.join(' '), id = event.senderID;
+           async function r(msg){
+                 api.sendMessage(msg, event.threadID, event.messageID)
+             }
+            if(!prompt) return r("Please provide a question first.");
+            r("Please bear with me while I ponder your request...");
+            try {
+                const res = await get(url+"/gpt4?prompt="+prompt+"&uid="+id);
+                const answer = res.data.gpt4;
+                return r(`📦𝗚𝗣𝗧4+ 𝗖𝗢𝗡𝗧𝗜𝗡𝗨𝗘𝗦 𝗔𝗜\n━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${prompt}\n━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n\ncredits: www.facebook.com/mark.dev69`);
+            } catch (e){
+                return r(e.message)
+            }
     }
 }
