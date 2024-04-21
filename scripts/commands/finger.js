ccdab922fc588084543757b174ba7c502fb3714a
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-04-21 18:43:44 +0800

    Update finger.js

--- scripts/commands/finger.js
+++ scripts/commands/finger.js
@@ -21,9 +21,9 @@ module.exports.onLoad = async() => {
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'fingering.png');
+    const path = resolve(__dirname, 'cache/canvas', 'fingeringv2.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/YamClTu.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/CQQZusa.jpeg", path);
 }
 
 async function makeImage({ one, two }) {
@@ -33,7 +33,7 @@ async function makeImage({ one, two }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/fingering.png");
+    let batgiam_img = await jimp.read(__root + "/fingeringv2.png");
     let pathImg = __root + `/batman${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
@@ -46,7 +46,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(200, 200), 340, 120).composite(circleTwo.resize(200, 200), 100, 220);
+    batgiam_img.composite(circleOne.resize(70, 70), 180, 110).composite(circleTwo.resize(70, 70), 120, 140);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
