979dd768850a3c4f978c406d015f88c9575b425c
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-04-21 22:05:22 +0800

    Update ryuko.js

--- ryuko.js
+++ ryuko.js
@@ -1,38 +1,93 @@
+const axios = require('axios');
+const cron = require('node-cron');
+
 module.exports = async ({ api }) => {
-  const logger = require('./ryuko/catalogs/ryukoc.js')
-  const configCustom = {
-    autoRestart: {
-      status: false,
-      time: 150, // 20 minutes
-      note: 'to avoid problems, enable periodic bot restarts, set the status to false if you want to disable auto restart function.'
-    },
-    accpetPending: {
-      status: false,
-      time: 10, // 10 minutes
-      note: 'approve waiting messages after a certain time, set the status to false if you want to disable auto accept message request.'
+  const messagedThreads = new Set();
+
+  async function sendMessage(body, threadID) {
+    try {
+      await api.sendMessage({ body }, threadID);
+      messagedThreads.add(threadID);
+    } catch (error) {
+      console.error("Error sending a message:", error);
     }
   }
-  function autoRestart(config) {
-    if(config.status) {
-      setInterval(async () => {
-        logger(`auto restart is processing, please wait.`, "kazuma lvl.69")
-        process.exit(1)
-      }, config.time * 60 * 1000)
+
+  async function sendMotivation() {
+    try {
+      const quoteResponse = await axios.get('https://api.quotable.io/random');
+      const quote = quoteResponse.data.content;
+      const formattedQuoteMessage = `
+🔔 𝖣𝖺𝗂𝗅𝗒 𝖬𝗈𝗍𝗂𝗏𝖺𝗍𝗂𝗈𝗇!
+
+${quote}
+
+- ${quoteResponse.data.author}
+`;
+      const threads = await api.getThreadList(25, null, ['INBOX']);
+      let i = 0;
+      let j = 0;
+
+      while (j < 20 && i < threads.length) {
+        const thread = threads[i];
+        if (thread.isGroup && thread.name !== thread.threadID && !messagedThreads.has(thread.threadID)) {
+          await sendMessage(formattedQuoteMessage, thread.threadID);
+          j++;
+          const currentThreadID = thread.threadID;
+          setTimeout(() => {
+            messagedThreads.delete(currentThreadID);
+          }, 1000);
+        }
+        i++;
+      }
+    } catch (error) {
+      console.error("Error sending motivation:", error);
     }
   }
-  function accpetPending(config) {
-    if(config.status) {
-      setInterval(async () => {
-          const list = [
-              ...(await api.getThreadList(1, null, ['PENDING'])),
-              ...(await api.getThreadList(1, null, ['OTHER']))
-          ];
-          if (list[0]) {
-              api.sendMessage('this thread is automatically approved by our system.', list[0].threadID);
-          }
-      }, config.time * 60 * 1000)
+
+  async function sendBibleVerse() {
+    try {
+      const verseResponse = await axios.get('https://labs.bible.org/api/?passage=random&type=json');
+      const verse = verseResponse.data[0];
+      const formattedVerseMessage = `
+🔔 𝖣𝖺𝗂𝗅𝗒 𝖡𝗂𝖻𝗅𝖾 𝖵𝖾𝗋𝗌𝖾:
+
+${verse.text}
+
+- ${verse.bookname} ${verse.chapter}:${verse.verse}
+`;
+      const threads = await api.getThreadList(25, null, ['INBOX']);
+      let i = 0;
+      let j = 0;
+
+      while (j < 20 && i < threads.length) {
+        const thread = threads[i];
+        if (thread.isGroup && thread.name !== thread.threadID && !messagedThreads.has(thread.threadID)) {
+          await sendMessage(formattedVerseMessage, thread.threadID);
+          j++;
+          const currentThreadID = thread.threadID;
+          setTimeout(() => {
+            messagedThreads.delete(currentThreadID);
+          }, 1000);
+        }
+        i++;
+      }
+    } catch (error) {
+      console.error("Error sending Bible verse:", error);
     }
   }
-  autoRestart(configCustom.autoRestart)
- accpetPending(configCustom.accpetPending)
+
+
+
+  // Schedule to send motivation every 30 minutes
+  cron.schedule('*/30 * * * *', sendMotivation, {
+    scheduled: false,
+    timezone: "Asia/Manila"
+  });
+
+  // Schedule to send Bible verse every day
+  cron.schedule('*/30 * * * *', sendBibleVerse, {
+    scheduled: true,
+    timezone: "Asia/Manila"
+  });
 };
