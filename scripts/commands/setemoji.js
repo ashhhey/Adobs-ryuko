059411b936186e88c97f0b3cd1c2b9574b784325
ashhhey <157706699+ashhhey@users.noreply.github.com>
2024-04-22 00:06:38 +0800

    Create setemoji.js

---
+++ scripts/commands/setemoji.js
@@ -0,0 +1,16 @@
+module.exports.config = {
+	name: "setemoji",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "bao",
+	description: "Đổi emoji trong nhóm",
+  usePrefix: true,
+	commandCategory: "group",
+	usages: "setemoji [emoji]",
+	cooldowns: 3
+};
+
+module.exports.run = async function({ api, event, args }) {
+	const emoji = args.join(" ")
+	api.changeThreadEmoji(`${args.join(" ")}`, event.threadID, event.messagaID);
+}
