6f4c0481a5e1fb493dfe7dcbb64a5d3121b9985a
かいと <160225758+AdoboWu@users.noreply.github.com>
2024-04-21 10:43:30 +0800

    Update ryukoa.js

--- ryuko/catalogs/ryukoa.js
+++ ryuko/catalogs/ryukoa.js
@@ -3,17 +3,18 @@ const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
 const chalk = require('chalk');
-const logger = require("./ryukoc.js");
+const logger = process.env.PORT || 3000;
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/website/ryuko.html'));
 });
 console.clear();
 function startBot(message) {
     (message) ? logger(message, "starting") : "";
-  const port = process.env.PORT || 3000;
-app.listen(port,() => {
-    console.log(`apps is listening port ${port}`);
+  console.log(chalk.bold.blue('DEPLOYING MAIN SYSTEM\n'));
+  logger.loader(`deploying app on port ${PORT}`);
+  app.listen(logger.loader(`app deployed on port ${PORT}`));
   const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "ryukob.js"], {
         cwd: __dirname,
         stdio: "inherit",
