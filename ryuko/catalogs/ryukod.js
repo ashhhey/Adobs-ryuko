487edd0b272f847172190b2280c9c9b393feee21
ashhhey <157706699+ashhhey@users.noreply.github.com>
2024-05-13 01:54:40 +0800

    Update ryukod.js

--- ryuko/catalogs/ryukod.js
+++ ryuko/catalogs/ryukod.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const os = require("os");
 const axios = require("axios");
-const config = require('../../ryuko.json');
+const config = require('../../ryuko.dev.json');
 const package = require('../../package.json');
 const FormData = require('form-data');
 const { resolve, basename } = require('path')
